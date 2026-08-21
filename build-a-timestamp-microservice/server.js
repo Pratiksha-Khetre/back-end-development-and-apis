bed9b1be6b5e4e3f22a70c543a2d34447ef46063
Pratiksha-Khetre <pratikshakhetre1011@gmail.com>
2026-08-21 09:48:47 +0000

    Complete next project

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -13,9 +13,40 @@ app.get("/", (_req, res) => {
 
 // Do not change code above this line
 
+app.get("/api", (req, res) => {
+  const date = new Date();
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api/:date", (req, res) => {
+  let date;
+
+  if (/^\d+$/.test(req.params.date)) {
+    date = new Date(Number(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({
+      error: "Invalid Date"
+    });
+  }
+
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
 // Do not change code below this line
 
 const PORT = 8000;
+
 const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
