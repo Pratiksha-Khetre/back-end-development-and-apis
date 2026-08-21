30011b9c10cb1e62bf5779a8116eda679ba2b9fb
Pratiksha-Khetre <pratikshakhetre1011@gmail.com>
2026-08-21 09:54:54 +0000

    Complete next project

--- build-a-chat-app/server.js
+++ build-a-chat-app/server.js
@@ -3,3 +3,71 @@ import fs from 'fs';
 import { WebSocketServer } from 'ws';
 
 const PORT = 3001;
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  fs.readFile('./public/index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    res.end(data);
+  });
+});
+
+// Create WebSocket server
+const wss = new WebSocketServer({ server });
+
+// Broadcast message to all connected clients
+function broadcast(message) {
+  const data = JSON.stringify(message);
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === 1) {
+      client.send(data);
+    }
+  });
+}
+
+// Handle WebSocket connections
+wss.on('connection', (socket, req) => {
+  const username = new URL(req.url, "http://localhost").searchParams.get(
+    "username",
+  );
+
+  // User joined
+  broadcast({
+    type: 'system',
+    text: `${username} joined`
+  });
+
+  // Receive chat message
+  socket.on('message', (message) => {
+    const { username, text } = JSON.parse(message.toString());
+
+    broadcast({
+      type: 'chat',
+      username,
+      text
+    });
+  });
+
+  // User disconnected
+  socket.on('close', () => {
+    broadcast({
+      type: 'system',
+      text: `${username} left`
+    });
+  });
+});
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Chat server running at http://localhost:3001`);
+});
